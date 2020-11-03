de305a8fdef553afaae556792a5a759fa708eb2f
Administrator <Administrator@DELL>
2020-11-03 20:40:02 +0530

    Project guide file is added.

--- SpringBootHelloWorldWithLogbackAndHTML/src/main/java/com/javaLive/main/SpringBootHelloWorldWithLogbackApplication.java
+++ SpringBootHelloWorldWithLogbackAndHTML/src/main/java/com/javaLive/main/SpringBootHelloWorldWithLogbackApplication.java
@@ -6,14 +6,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringBootHelloWorldWithJspApplication {
-	private static final Logger LOGGER=LoggerFactory.getLogger(SpringBootHelloWorldWithJspApplication.class);
+public class SpringBootHelloWorldWithLogbackApplication {
+	private static final Logger LOGGER=LoggerFactory.getLogger(SpringBootHelloWorldWithLogbackApplication.class);
 	public static void main(String[] args) {
-		SpringApplication.run(SpringBootHelloWorldWithJspApplication.class, args);
+		SpringApplication.run(SpringBootHelloWorldWithLogbackApplication.class, args);
 		LOGGER.info("=========This is info level log message.===================");
 		LOGGER.trace("=========This is trace level log message.===================");
         LOGGER.debug("=========This is debug level log message.===================");
-       // LOGGER.info("doStuff took input - {}", 3);
         LOGGER.warn("=========This is warn level log message.===================");
         LOGGER.error("=========This is error level log message.===================");
 	}
--- SpringBootHelloWorldWithLogbackAndHTML/src/test/java/com/javaLive/main/SpringBootHelloWorldWithLogbackApplicationTests.java
+++ SpringBootHelloWorldWithLogbackAndHTML/src/test/java/com/javaLive/main/SpringBootHelloWorldWithLogbackApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SpringBootHelloWorldWithJspApplicationTests {
+class SpringBootHelloWorldWithLogbackApplicationTests {
 
 	@Test
 	void contextLoads() {
